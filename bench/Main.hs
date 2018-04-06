475047318c33bf9bbd66ac5814efb092fadc4fb5
Robin KAY <komadori@gekkou.co.uk>
2018-04-06 22:27:18 +0000

    Add equality test micro-benchmark.

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Data.Proxy
+import Data.Typeable
 import Data.Word
 import Data.Word.Odd
 import Criterion.Main
@@ -7,13 +11,19 @@ import Criterion.Main
 testAddMul :: (Num a) => a -> a
 testAddMul n = 2*n*n + 3*n + 4
 
+testEquals :: (Eq a, Num a) => a -> Bool
+testEquals n = n == 7 || n == 11 || n == 13
+
+benchNum :: forall a. (Eq a, Num a, Typeable a) => a -> Benchmark
+benchNum x =
+    bgroup (show $ typeRep (Proxy :: Proxy a)) [
+        bench "addMul" $ whnf testAddMul x,
+        bench "equals" $ whnf testAddMul x
+    ]
+
 main :: IO ()
 main =
     defaultMain [
-        bgroup "Word" [
-            bench "addMul" $ whnf testAddMul (1::Word)
-        ],
-        bgroup "Word20" [
-            bench "addMul" $ whnf testAddMul (1::Word20)
-        ]
+        benchNum (1::Word),
+        benchNum (1::Word20)
     ]
